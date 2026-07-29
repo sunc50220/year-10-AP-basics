5aa65afc30a3133621bbec59f0a418c9518656a5
sunc50220 <sunc50220@masseyhigh.school.nz>
2026-07-30 10:46:40 +1200

    working area / perimeter

---
+++ P_04_get_width.py
@@ -0,0 +1,17 @@
+# Ask user for width and loop until they
+# enter a number that is more than zero
+
+error = "please enter a number that is more than zero\n"
+while True:
+
+    try:
+        # ask the user for a number
+        width = float(input("width: "))
+        # check that the number is more than zero
+        if width > 0:
+            break
+        else:
+            print(error)
+
+    except ValueError:
+        print(error)
